345d4c834fae0c8712ede786b60279a4adc91c29
곽현민 <hunmin111@gmail.com>
2026-01-13 13:40:23 +0900

    init

---
+++ src/main/java/com/firstevent/FirsteventApplication.java
@@ -0,0 +1,13 @@
+package com.firstevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsteventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirsteventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/firstevent/FirsteventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsteventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
