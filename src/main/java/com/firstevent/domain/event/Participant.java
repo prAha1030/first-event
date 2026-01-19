55b95ca2e9b5a937bf587fd4b23ac38619df2b9f
곽현민 <hunmin111@gmail.com>
2026-01-19 20:35:16 +0900

    feat: 이벤트, 참여 도메인 구현

--- src/main/java/com/firstevent/domain/event/Participant.java
+++ src/main/java/com/firstevent/domain/event/Participant.java
@@ -2,18 +2,27 @@ package com.firstevent.domain.event;
 
 import com.firstevent.domain.member.Member;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Participant {
     private Member member;
     private Event event;
+    private boolean isWinner;
     private LocalDateTime participateAt;
 
-    public Participant(Member member, Event event) {
-        this.member = member;
-        this.event = event;
-        participateAt = LocalDateTime.now();
+    public static Participant regist(Member member, Event event, Determination determination) {
+        Participant participant = new Participant();
+
+        participant.member = member;
+        participant.event = event;
+        participant.isWinner = determination.determinate();
+        participant.participateAt = LocalDateTime.now();
+
+        return participant;
+
     }
 }
