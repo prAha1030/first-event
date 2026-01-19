606577d3c3beb90cf17068d34e63c0c8ffc6c13d
곽현민 <hunmin111@gmail.com>
2026-01-19 20:43:03 +0900

    feat: 참여 도메인 Entity화

--- src/main/java/com/firstevent/domain/event/Participant.java
+++ src/main/java/com/firstevent/domain/event/Participant.java
@@ -1,24 +1,35 @@
 package com.firstevent.domain.event;
 
 import com.firstevent.domain.member.Member;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(name = "uk_participant_event", columnNames = {"memberId", "eventId"})})
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Participant {
-    private Member member;
-    private Event event;
+    // 참여 식별자, 멤버, 이벤트, 당첨, 참여일
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private Long memberId;
+    @Column(nullable = false, insertable = false, updatable = false)
+    private Long eventId;
+    @Column(nullable = false)
     private boolean isWinner;
+    @Column(nullable = false)
     private LocalDateTime participateAt;
 
     public static Participant regist(Member member, Event event, Determination determination) {
         Participant participant = new Participant();
 
-        participant.member = member;
-        participant.event = event;
+        participant.memberId = member.getId();
+        participant.eventId = event.getId();
         participant.isWinner = determination.determinate();
         participant.participateAt = LocalDateTime.now();
 
