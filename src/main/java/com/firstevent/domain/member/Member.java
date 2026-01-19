81063da7c321677c4312a80dcd5dcc2143ab9567
곽현민 <hunmin111@gmail.com>
2026-01-19 20:27:53 +0900

    feat: 멤버 도메인 설계

--- src/main/java/com/firstevent/domain/member/Member.java
+++ src/main/java/com/firstevent/domain/member/Member.java
@@ -1,10 +1,13 @@
 package com.firstevent.domain.member;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     private String email;
     private String password;
@@ -13,12 +16,19 @@ public class Member {
     private MemberRole role;
     private LocalDateTime registAt;
 
-    public Member(String email, String password, String nickname) {
+    public Member(String email, String password, String nickname, PasswordEncoder passwordEncoder) {
         this.email = email;
-        this.password = password;
+        this.password = passwordEncoder.encode(password);
         this.nickname = nickname;
         this.status = MemberStatus.ACTIVE;
         this.role = MemberRole.USER;
         registAt = LocalDateTime.now();
+    }
+    public static Member regist(String email, String password, String nickname, PasswordEncoder passwordEncoder) {
+        return new Member(email, password, nickname, passwordEncoder);
+    }
+
+    public void withdraw() {
+        this.status = MemberStatus.INACTIVE;
     }
 }
